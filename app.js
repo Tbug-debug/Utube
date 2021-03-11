4ad4666a40cbd688f725f3add7889fee3638f0f6
Tbug-debug <lhg6325@gmail.com>
2021-03-11 14:22:15 +0900

    Master3

--- app.js
+++ app.js
@@ -3,17 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
-
-
-const handleHome = (req, res) => res.send('Hello Fucker!');
-
-
-const handleProfile = (req, res) => res.send("You are on Profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -21,8 +15,9 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
 
-app.get('/profile', handleProfile);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
-app.listen(PORT, handleListening);
+export default app;
